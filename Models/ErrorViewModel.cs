fbeca4fb6237cdec1133770cebfe2d59dedce765
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2021-08-15 21:29:51 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace babe_algorithms.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
