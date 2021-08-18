79785c479a5bfb14290502a2e0f5d79136f2fbbe
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2021-08-18 11:15:15 -0400

    2 new recipes

--- Program.cs
+++ Program.cs
@@ -67,6 +67,25 @@ namespace babe_algorithms
             var agave = new Ingredient() { Name = "Agave" };
             var driedBlueberries = new Ingredient() { Name = "Dried blueberries" };
             var sliveredAlmonds = new Ingredient() { Name = "Slivered almonds" };
+            var russetPotatoes = new Ingredient() { Name = "Russet potatoes" };
+            var brocolliFlorets = new Ingredient() { Name = "Brocolli florets" };
+            var bread = new Ingredient() { Name = "Bread" };
+            var garlicCloves = new Ingredient() { Name = "Garlic cloves" };
+            var butter = new Ingredient() { Name = "Butter" };
+            var whiteMisoPaste = new Ingredient() { Name = "White miso paste" };
+            var yellowOnion = new Ingredient() { Name = "Yellow onion" };
+            var nutritionalYeast = new Ingredient() { Name = "Nutritional yeast" };
+            var vegetableBrothConcentrate = new Ingredient() { Name = "Vegetable broth concentrate" };
+            var pieCrust = new Ingredient() { Name = "Pie crust" };
+            var tofu = new Ingredient() { Name = "Tofu" };
+            var onionCeleyCarrotMix = new Ingredient() { Name = "Onion, celery, carrot mix" };
+            var peas = new Ingredient() { Name = "Peas" };
+            var allPurposeFlour = new Ingredient() { Name = "All purpose flour" };
+            var soySauce = new Ingredient() { Name = "Soy sauce" };
+            var vegetableBroth = new Ingredient() { Name = "Vegetable broth" };
+            var sage = new Ingredient() { Name = "Sage" };
+            var thyme = new Ingredient() { Name = "Thyme" };
+
             context.Ingredients.AddRange(new Ingredient[]
             {
                 poppySeeds,
@@ -76,6 +95,24 @@ namespace babe_algorithms
                 agave,
                 driedBlueberries,
                 sliveredAlmonds,
+                russetPotatoes,
+                brocolliFlorets,
+                bread,
+                garlicCloves,
+                butter,
+                whiteMisoPaste,
+                yellowOnion,
+                nutritionalYeast,
+                vegetableBrothConcentrate,
+                pieCrust,
+                tofu,
+                onionCeleyCarrotMix,
+                peas,
+                allPurposeFlour,
+                soySauce,
+                vegetableBroth,
+                sage,
+                thyme,
             });
 
             var breakfastCategory =
@@ -83,8 +120,15 @@ namespace babe_algorithms
                 {
                     Name = "Breakfast"
                 };
+            var entreeCategory =
+                new Category()
+                {
+                    Name = "Entree"
+                };
+
             context.Categories.AddRange(
-                breakfastCategory
+                breakfastCategory,
+                entreeCategory
             );
 
             var overnightOatsRecipe = new Recipe()
@@ -159,6 +203,234 @@ namespace babe_algorithms
                 }
             };
             context.Recipes.Add(overnightOatsRecipe);
+
+            var broccoliCheddarSoup = new Recipe()
+            {
+                Name = "Broccoli Cheddar Soup",
+                Cooktime = TimeSpan.FromMinutes(40),
+                ServingsProduced = 4,
+                CaloriesPerServing = 490.0,
+                Categories = new HashSet<Category>()
+                {
+                    entreeCategory,
+                },
+                Ingredients = new List<IngredientRequirement>()
+                {
+                    new IngredientRequirement()
+                    {
+                        Ingredient = russetPotatoes,
+                        Quantity = 2,
+                        Unit = Unit.Count,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = brocolliFlorets,
+                        Quantity = 20,
+                        Unit = Unit.Ounce,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = bread,
+                        Quantity = 2,
+                        Unit = Unit.Count,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = garlicCloves,
+                        Quantity = 6,
+                        Unit = Unit.Count,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = butter,
+                        Quantity = 6,
+                        Unit = Unit.Tablespoon,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = whiteMisoPaste,
+                        Quantity = 2,
+                        Unit = Unit.Tablespoon,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = yellowOnion,
+                        Quantity = 2,
+                        Unit = Unit.Count,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = nutritionalYeast,
+                        Quantity = 4,
+                        Unit = Unit.Tablespoon,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = vegetableBrothConcentrate,
+                        Quantity = 4,
+                        Unit = Unit.Teaspoon,
+                    },
+                }
+            };
+            broccoliCheddarSoup.Steps = new List<RecipeStep>()
+            {
+                new RecipeStep()
+                {
+                    Text = "Preheat over to 425F. Peel and dice the potato. Roughly chop the broccoli florets into bite-size pieces. Add just 1 cup diced potato to a large pot, cover with 1 inch of water, and add a pinch of salt. Bring to a boil and add just 4 cups chopped broccoli florets. Cover and cook until the potatoes are fork-tender, about 10 to 12 minutes. Drain and transfer vegetables to a blender.",
+                },
+                new RecipeStep()
+                {
+                    Text = "Add remaining diced potato and remaining chopped broccoli florets to a baking sheet and toss with 2 Tbsp olive oil and a pinch of salt and pepper. Roast until tender, about 15 to 17 minutes."
+                },
+                new RecipeStep()
+                {
+                    Text = "Cut the bread roll into 1/2 inch cubes. Peel and mince the garlic. Add the butter, white miso paste, and just half the minced garlic to a bowl and mix well. Add the cubed bread to a baking sheet and bake until golden brown, about 10 to 12 minutes."
+                },
+                new RecipeStep()
+                {
+                    Text = "Peel and dice the onion. Place a medium skillet over medium-high heat with 2 Tbsp olive oil. Once the oil is hot, add the diced onion and remaining minced garlic. Cook until softened, about 3 to 5 minutes, and transfer to the blender with the boiled potatoes and broccoli."
+                },
+                new RecipeStep()
+                {
+                    Text = "Add the nutritional yeast, vegetable broth concentrate, 2 cups water, 2 tsp salt, and a pinch of pepper to the blender. Blend the broccoli cheddar soup until smooth, making sure to vent the blender for steam to escape. If necessary, blend the soup in batches, don't overfill the blender."
+                },
+                new RecipeStep()
+                {
+                    Text = "Divide the soup between bowls, top with roasted potatoes, roasted broccoli, and miso garlic croutons."
+                },
+            };
+            context.Recipes.Add(broccoliCheddarSoup);
+
+            var vegetablePotPie = new Recipe()
+            {
+                Name = "Vegetable Pot Pie",
+                Cooktime = TimeSpan.FromMinutes(60),
+                ServingsProduced = 8,
+                Categories = new HashSet<Category>() { entreeCategory },
+                Ingredients = new List<IngredientRequirement>()
+                {
+                    new IngredientRequirement()
+                    {
+                        Ingredient = pieCrust,
+                        Quantity = 2,
+                        Unit = Unit.Count,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = tofu,
+                        Quantity = 10,
+                        Unit = Unit.Ounce,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = onionCeleyCarrotMix,
+                        Quantity = 20,
+                        Unit = Unit.Ounce,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = peas,
+                        Quantity = 0.5,
+                        Unit = Unit.Cup,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = garlicCloves,
+                        Quantity = 2,
+                        Unit = Unit.Count,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = russetPotatoes,
+                        Quantity = 1,
+                        Unit = Unit.Count,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = allPurposeFlour,
+                        Quantity = 0.25,
+                        Unit = Unit.Cup,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = nutritionalYeast,
+                        Quantity = 2,
+                        Unit = Unit.Teaspoon,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = soySauce,
+                        Quantity = 2,
+                        Unit = Unit.Tablespoon,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = vegetableBroth,
+                        Quantity = 1.25,
+                        Unit = Unit.Cup,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = sage,
+                        Quantity = 1,
+                        Unit = Unit.Teaspoon,
+                    },
+                    new IngredientRequirement()
+                    {
+                        Ingredient = thyme,
+                        Quantity = 1,
+                        Unit = Unit.Teaspoon,
+                    },
+                }
+            };
+            vegetablePotPie.Steps = new List<RecipeStep>()
+            {
+                new RecipeStep()
+                {
+                    Text = "Preheat oven to 400° F.",
+                },
+                new RecipeStep()
+                {
+                    Text = "Cut tofu into 1/3-inch dice and press between clean kitchen towels or paper towels to rid of excess water.",
+                },
+                new RecipeStep()
+                {
+                    Text = "Heat 1 tablespoon of olive oil in a large skillet over medium heat and cook tofu until golden on all, or most, sides. Remove tofu from skillet and set aside.",
+                },
+                new RecipeStep()
+                {
+                    Text = "Heat remaining 2 tablespoons of olive oil in the same skillet. Add onion, celery, carrot, and garlic and sauté until onion is translucent.",
+                },
+                new RecipeStep()
+                {
+                    Text = "Add potato to the skillet and cook, stirring frequently, until tender but not mushy.",
+                },
+                new RecipeStep()
+                {
+                    Text = "Add flour, nutritional yeast, and soy sauce to the skillet and stir into the vegetables.",
+                },
+                new RecipeStep()
+                {
+                    Text = "Add vegetable broth and stir until combined, scraping all the browned bits from the bottom of the pan.",
+                },
+                new RecipeStep()
+                {
+                    Text = "Add tofu, peas, sage, and thyme and stir until combined.",
+                },
+                new RecipeStep()
+                {
+                    Text = "Remove from heat and season to taste with salt and pepper.",
+                },
+                new RecipeStep()
+                {
+                    Text = "Prepare pie crust according to packaging instructions. Fill pie with tofu and vegetable mixture. Place dough over the filling to complete pot pie, press to seal around the edges of the dishes and crimp with a fork. Cut a slit in the middle.",
+                },
+                new RecipeStep()
+                {
+                    Text = "Bake in the oven until golden, about 30 minutes.",
+                },
+            };
+            context.Recipes.Add(vegetablePotPie);
             context.SaveChanges();
         }
     }
