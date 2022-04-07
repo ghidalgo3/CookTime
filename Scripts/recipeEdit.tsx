efe63cf211f4ed8f8ed1699b407d72bceef1c414
Cam <sayurimila3@gmail.com>
2022-04-07 01:18:45 -0400

    nav bar

--- Scripts/recipeEdit.tsx
+++ Scripts/recipeEdit.tsx
@@ -476,12 +476,12 @@ class RecipeEdit extends React.Component<RecipeEditProps, RecipeEditState>
 
     toTitleCase(str) {
         return str.replace(
-          /\w\S*/g,
-          function(txt) {
+            /\w\S*/g,
+            function(txt) {
             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-          }
+            }
         );
-      }
+    }
 
     nutritionFacts() {
         // return null;
