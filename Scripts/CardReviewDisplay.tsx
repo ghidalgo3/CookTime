436d7b12063d0889efa4c855119659351758b222
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2022-04-30 23:31:48 -0400

    show same ratings in index

---
+++ Scripts/CardReviewDisplay.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Rating from "react-rating";
+
+type CardReviewDisplayProps = {
+    averageReviews : number,
+    reviewCount : number
+}
+
+class CardReviewDisplay extends React.Component<CardReviewDisplayProps, {}> {
+    constructor(props : CardReviewDisplayProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <Rating
+                    initialRating={this.props.averageReviews}
+                    readonly
+                    emptySymbol="far fa-star"
+                    fullSymbol="fas fa-star"
+                    fractions={2} />
+                ({this.props.reviewCount})
+            </div>
+        )
+    }
+}
+
+var recipeCards = document.querySelectorAll(".recipe-card")
+recipeCards.forEach(recipeCard => {
+    let averageReviews = Number.parseFloat(recipeCard.getAttribute("data-average-reviews") as string)
+    let reviewCount = Number.parseInt(recipeCard.getAttribute("data-review-count") as string)
+    var target = recipeCard.querySelector("#rating-target")
+    ReactDOM.render(
+        <CardReviewDisplay averageReviews={averageReviews} reviewCount={reviewCount} />,
+        target
+    )
+});
