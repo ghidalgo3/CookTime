438f186a652370bd8e54820dae89ab84584359e0
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2023-01-12 21:20:01 -0500

    cooktime banner

---
+++ client-app/src/components/CookTimeBanner.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react"
+
+function CookTimeBanner() {
+  return (
+  <section className="py-5 text-center container margin-bottom-10">
+    <div className="row py-lg-5">
+      <h1 className="fw-light">The CookTime Experience</h1>
+      <p className="lead text-muted">CookTime is the recipe organizer you have been waiting for.</p>
+    </div>
+  </section>);
+}
+export default CookTimeBanner;
