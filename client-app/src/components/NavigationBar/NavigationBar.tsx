9874264ec8e7642daee28f978fd2249ab1304fec
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2023-01-12 21:19:48 -0500

    initial navigation bar

---
+++ client-app/src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,67 @@
+import React, {useEffect, useState} from"react"
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { isMainThread } from "worker_threads";
+import imgs from "../../assets";
+import "./NavigationBar.css"
+
+export interface NavigationBarProps {
+}
+
+function NavigationBar(props: NavigationBarProps | null) {
+    return (
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand id="my-navbar-brand" href="#home">
+          <img
+              alt=""
+              src={imgs.logo}
+              height="30"
+              className="d-inline-block align-middle"
+            />{' '}
+            CookTime
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Recipes</Nav.Link>
+              <NavDropdown title="Categories" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#home">Groceries List</Nav.Link>
+              <Nav.Link href="#home">Blog</Nav.Link>
+              <NavDropdown title="Admin" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="User Id" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>);
+}
+export default NavigationBar;
