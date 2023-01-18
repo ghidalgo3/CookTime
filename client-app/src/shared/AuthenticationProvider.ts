89a1e2a6d7bf992d50b35f65d655adcb1b55f088
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2023-01-17 21:46:09 -0500

    implement the authentication provider

--- client-app/src/shared/AuthenticationProvider.ts
+++ client-app/src/shared/AuthenticationProvider.ts
@@ -1,17 +1,41 @@
-export const AuthenticationProvider = {
-  isAuthenticated: false,
-  /**
-   * 
-   * @param callback Called after successfully signin in
-   */
-  signIn(callback: () => void) {
-    // fetch to signin
-  },
-  /**
-   * 
-   * @param callback Called after successfully signin out
-   */
-  signOut(callback: () => void) {
+type Role = "User" | "Administrator";
 
+interface UserData {
+  name: string,
+  roles: Role[]
+}
+
+type SignInResult = "Success" | "Failure"
+
+interface IAuthenticationProvider {
+  user : UserData | null,
+
+  signIn(
+    usernameOrEmail : string,
+    password : string,
+    rememberMe: boolean) : Promise<SignInResult>,
+
+  signOut() : Promise<void>,
+}
+
+export const AuthenticationProvider : IAuthenticationProvider = {
+  user: null,
+  signIn: async function (usernameOrEmail: string, password: string, rememberMe: boolean): Promise<SignInResult> {
+    let formData = new FormData();
+  formData.append('userNameOrEmail', usernameOrEmail);
+  formData.append('password', password);
+    const response = await fetch("/api/account/signin", {
+      method: "post",
+      body: formData
+    })
+    if (response.ok) {
+      this.user = await response.json()
+      return "Success"
+    } else {
+      return "Failure"
+    }
+  },
+  signOut: function (): Promise<void> {
+    throw new Error("Function not implemented.");
   }
 }
