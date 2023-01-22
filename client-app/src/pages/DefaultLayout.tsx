0dd107c7de2cc2091798a9f93221f0b414e9bacc
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2023-01-22 18:07:24 -0500

    add footer to default layout

--- client-app/src/pages/DefaultLayout.tsx
+++ client-app/src/pages/DefaultLayout.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from"react"
 import { Outlet, useLocation } from "react-router-dom";
 import { CookTimeBanner, NavigationBar, SignUp } from "src/components";
+import Footer from "src/components/Footer";
 
 export default function DefaultLayout() {
   const location = useLocation();
@@ -11,6 +12,7 @@ export default function DefaultLayout() {
     {/* How to render the banner only at the home page */}
     {location.pathname === "/" && <CookTimeBanner />}
     <Outlet />
+    <Footer />
     </>
   );
 }
