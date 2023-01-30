3e5b4c4b120172f60017c95f18ab71531b22ff91
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2023-01-29 22:26:56 -0500

    navbar collapses at correct breakpoint

--- client-app/src/components/NavigationBar/NavigationBar.tsx
+++ client-app/src/components/NavigationBar/NavigationBar.tsx
@@ -46,7 +46,7 @@ export function NavigationBar({ categories }: { categories: string[] }) {
 
   return (
     <header>
-      <Navbar expand="lg">
+      <Navbar expand="sm">
         <Container fluid>
           <Link to="/">
             <Navbar.Brand id="my-navbar-brand">
