c256ee196b60a4e4b1517be0ae563640991399fe
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2023-06-11 23:09:36 -0400

    IN-feed ads component

---
+++ client-app/src/components/GoogleInFeedAds.tsx
@@ -0,0 +1,16 @@
+export function GoogleInFeedAds() {
+  return (
+    <>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6004231239349931"
+        crossOrigin="anonymous"></script>
+      <ins className="adsbygoogle"
+        style={{"display":"block", "textAlign":"center"}}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-6004231239349931"
+        data-ad-slot="5207463487"></ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({ });
+      </script>
+    </>)
+}
