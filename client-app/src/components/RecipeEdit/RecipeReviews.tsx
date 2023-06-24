10dd1d8c85e948084712688d02dc7088c7fe1b91
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2023-06-24 00:42:25 -0400

    Avoid reload on review delete

--- client-app/src/components/RecipeEdit/RecipeReviews.tsx
+++ client-app/src/components/RecipeEdit/RecipeReviews.tsx
@@ -28,9 +28,9 @@ export function RecipeReviews({recipeId} : RecipeReviewsProps) {
       if (response.ok) {
         // TODO this cannot be a location.reload()
         // eslint-disable-next-line no-restricted-globals
-        location.reload();
-      }
-    });
+        // location.reload();
+        setReviews(reviews.filter(review => review.owner.id !== user?.id));
+    }});
   }
 
   return (
