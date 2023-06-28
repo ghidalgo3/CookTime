811f904b216b32200bd9b0bf726fce05cd7ae3d6
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2023-06-28 13:30:57 -0400

    commit tests

--- Tests/ChatGPTTests.cs
+++ Tests/ChatGPTTests.cs
@@ -1,18 +1,79 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace babe_algorithms;
+namespace babe_algorithms.Tests;
 
 [TestClass]
 // [Ignore]
 public class ChatGPTTests
 {
-    [TestMethod]
-    public void Smoke()
+
+    public static IRecipeArtificialIntelligence GetChatGPT()
     {
         IRecipeArtificialIntelligence ai = new ChatGPT(new OpenAIOptions()
         {
-            Key = ""
+            Key = Environment.GetEnvironmentVariable("OpenAI__Key")
         }, null);
-        Assert.IsTrue(ai.ConvertToRecipeAsync("5 grams of salt\n1/4 cups of milk\n10 milliliters water", CancellationToken.None).Result != null);
+        return ai;
+    }
+
+
+    [TestMethod]
+    public void NonRecipe()
+    {
+        var ai = GetChatGPT();
+        var input =
+"""
+Not a recipe
+""";
+
+        var recipe = ai.ConvertToRecipeAsync(input, CancellationToken.None).Result;
+        Assert.IsNotNull(recipe);
+        // Assert.IsNotNull(recipe);
+    }
+
+    [TestMethod]
+    public void SimpleRecipeParse()
+    {
+        var ai = GetChatGPT();
+        var input =
+"""
+My crazy recipe
+Serves 3
+40 minutes to prepare
+
+Ingredients:
+5 grams kosher salt
+1/4 cups of whole milk
+10 milliliters water
+4 garlic clove, minced
+
+Steps:
+Mix everything together in a blender.
+Serve and enjoy.
+""";
+
+        var recipe = ai.ConvertToRecipeAsync(input, CancellationToken.None).Result;
+        Assert.AreEqual("My Crazy Recipe", recipe.Name);
+        Assert.AreEqual(4, recipe.GetAllIngredients().Count);
+        Assert.AreEqual(3, recipe.ServingsProduced);
+        Assert.AreEqual(40, recipe.CooktimeMinutes);
+
+        Assert.AreEqual(5, recipe.RecipeComponents[0].Ingredients[0].Quantity);
+        Assert.AreEqual(Unit.Gram, recipe.RecipeComponents[0].Ingredients[0].Unit);
+        Assert.AreEqual("kosher salt", recipe.RecipeComponents[0].Ingredients[0].Ingredient.Name);
+
+        Assert.AreEqual(0.25, recipe.RecipeComponents[0].Ingredients[1].Quantity);
+        Assert.AreEqual(Unit.Cup, recipe.RecipeComponents[0].Ingredients[1].Unit);
+        Assert.AreEqual("whole milk", recipe.RecipeComponents[0].Ingredients[1].Ingredient.Name);
+
+        Assert.AreEqual(10, recipe.RecipeComponents[0].Ingredients[2].Quantity);
+        Assert.AreEqual(Unit.Milliliter, recipe.RecipeComponents[0].Ingredients[2].Unit);
+        Assert.AreEqual("water", recipe.RecipeComponents[0].Ingredients[2].Ingredient.Name);
+
+        Assert.AreEqual(4, recipe.RecipeComponents[0].Ingredients[3].Quantity);
+        Assert.AreEqual(Unit.Count, recipe.RecipeComponents[0].Ingredients[3].Unit);
+        Assert.AreEqual("garlic clove", recipe.RecipeComponents[0].Ingredients[3].Ingredient.Name);
+
+        Assert.AreEqual(2, recipe.RecipeComponents[0].Steps.Count);
     }
 }
---
+++ Tests/ImageToRecipeTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace babe_algorithms.Tests;
+
+[TestClass]
+public class ImageToRecipeTests
+{
+    public static IComputerVision GetComputerVision()
+    {
+        return new AzureCognitiveServices(new AzureOptions()
+        {
+            VisionEndpoint = "https://cooktime.cognitiveservices.azure.com/",
+            VisionKey = Environment.GetEnvironmentVariable("Azure__VisionKey"),
+        });
+    }
+
+    [TestMethod]
+    public async Task LemonyPastaWithChickpeas()
+    {
+        var ai = ChatGPTTests.GetChatGPT();
+        var cv = GetComputerVision();
+
+        var fileStream = File.OpenRead("Tests/lemony pasta with chickpeas.png");
+        var text = await cv.GetTextAsync(fileStream, CancellationToken.None);
+        var recipe = await ai.ConvertToRecipeAsync(text, CancellationToken.None);
+        Assert.AreEqual("Lemony Pasta With Chickpeas", recipe.Name);
+        Assert.AreEqual(4, recipe.ServingsProduced);
+        Assert.AreEqual(3, recipe.RecipeComponents[0].Steps.Count);
+        Assert.AreEqual(12, recipe.RecipeComponents[0].Ingredients.Count);
+        Assert.AreEqual(0.5, recipe.RecipeComponents[0].Ingredients[0].Quantity);
+    }
+}
