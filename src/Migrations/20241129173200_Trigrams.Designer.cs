584ecaaa536df6f2a8ec4f8d80a28ec445c1c392
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2024-11-29 12:42:16 -0500

    Trigram migration

--- src/Migrations/20241129173200_Trigrams.Designer.cs
+++ src/Migrations/20241129173200_Trigrams.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -14,9 +15,11 @@ using babe_algorithms.Services;
 namespace babe_algorithms.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241129173200_Trigrams")]
+    partial class Trigrams
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Migrations/20241129173200_Trigrams.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace babe_algorithms.Migrations
+{
+    /// <inheritdoc />
+    public partial class Trigrams : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE EXTENSION IF NOT EXISTS pg_trgm;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP EXTENSION IF EXISTS pg_trgm;
+            ");
+        }
+    }
+}
