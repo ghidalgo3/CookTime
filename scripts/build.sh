f410eb534f99112676c6a89a3878910bcf5aa84e
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2026-02-01 18:00:23 -0500

    Clean up code build and publish (#81)

--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Building the solution..."
-dotnet build
--- scripts/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-OUTPUT_PATH=${1:-"./publish"}
-
-echo "Publishing to $OUTPUT_PATH..."
-cd src
-dotnet publish --configuration Release --verbosity normal --output "$OUTPUT_PATH"
