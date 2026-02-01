c5944f4a3105a9684f4ab0506714f83fa79b5c70
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2026-02-01 12:57:33 -0500

    reorganize files

---
+++ src/CookTime/wwwroot/assets/CatchAll-DOv3m6s6.js
@@ -0,0 +1,1 @@
+import{w as t,j as a,N as o}from"./chunk-EPOLDU6W-43oXRp_d.js";const s=t(function(){return a.jsx(o,{to:"/",replace:!0})});export{s as default};
---
+++ src/CookTime/wwwroot/assets/useTitle-Da5jpf3t.js
@@ -0,0 +1,1 @@
+import{r as o}from"./chunk-EPOLDU6W-43oXRp_d.js";function i(e){o.useEffect(()=>{e?document.title=e+" - CookTime":document.title="CookTime"})}export{i as u};
